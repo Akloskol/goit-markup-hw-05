04dcc3216127cf5ed0c66dcb5836079381a1655a
Duseldof777 <va.madunov@gmail.com>
2023-05-08 01:19:38 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,14 @@
-const modalBackground = document.querySelector('.modal-background');
-const modalToggle = document.querySelectorAll('[data-modal]');
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-function toggleModal() {
-  modalBackground.classList.toggle('is-hidden');
-}
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-modalToggle.forEach(toggle => {
-  toggle.addEventListener('click', toggleModal);
-});
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
